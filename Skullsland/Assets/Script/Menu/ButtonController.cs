a90f9a77309dffd6effac3357000e10f9f1e525c
DrakenXD <79544585+DrakenXD@users.noreply.github.com>
2021-06-01 02:06:08 -0300

    Fazendo Sistema de save volume

---
+++ Skullsland/Assets/Script/Menu/ButtonController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour,IPointerEnterHandler,IPointerClickHandler,IPointerExitHandler
+{
+    [Header("Start")]
+    public bool start;
+    public string nameScene;
+
+    [Header("Options")]
+    public bool options;
+    public GameObject Activate;
+    public GameObject Disable;
+
+    [Header("Quit")]
+    public bool quit;
+
+
+    
+    private Vector3 sizeNormal = new Vector3(1,1,1);
+    [Header("Size Button")]
+    public Vector3 sizeBig;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.localScale = sizeNormal;
+
+        if (start)
+        {
+            SceneManager.LoadScene(nameScene);
+        }else if (options)
+        {
+            Disable.SetActive(false);
+            Activate.SetActive(true);
+        }
+        else if (quit)
+        {
+            Application.Quit();
+        }
+
+
+        Debug.Log("dsadas");
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = sizeBig;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = sizeNormal;
+    }
+
+   
+}
---
+++ Skullsland/Assets/Script/Menu/VolumeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeController : MonoBehaviour
+{
+    [SerializeField] private Slider MusicvolumeSlider = null;
+    [SerializeField] private AudioMixer MusicMixer;
+    [SerializeField] private Slider SoundEffectvolumeSlider = null;
+    [SerializeField] private AudioMixer SoundEffectMixer;
+
+    [SerializeField] new AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicLoadValues();
+        SoundEffectLoadValues();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            audio.Play();
+        }
+
+    }
+    public void MusicSaveVolumeButton()
+    {
+        float volumeValue = MusicvolumeSlider.value;
+
+        PlayerPrefs.SetFloat("Music",volumeValue);
+
+        MusicMixer.SetFloat("Music", Mathf.Log10(volumeValue) * 20);
+
+        MusicLoadValues();
+    }
+    void MusicLoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("Music");
+        MusicvolumeSlider.value = volumeValue; 
+    }
+
+    public void SoundEffectSaveVolumeButton()
+    {
+        float volumeValue = SoundEffectvolumeSlider.value;
+
+        PlayerPrefs.SetFloat("SoundEffects", volumeValue);
+
+        SoundEffectMixer.SetFloat("SoundEffects", Mathf.Log10(volumeValue) * 20);
+
+        SoundEffectLoadValues();
+    }
+    void SoundEffectLoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("SoundEffects");
+        SoundEffectvolumeSlider.value = volumeValue;
+      
+    }
+}
